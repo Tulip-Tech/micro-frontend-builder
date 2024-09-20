a6ae572dbafd10d5282e2245518181c497895f1a
mmarifat <mma.rifat66@gmail.com>
2024-09-20 15:40:20 +0100

    chore: added 10mb limit

--- pages/api/send-email.ts
+++ pages/api/send-email.ts
@@ -71,9 +71,9 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
 }
 
 export const config = {
-  api: {
-    bodyParser: {
-      sizeLimit: '10mb',
+    api: {
+        bodyParser: {
+            sizeLimit: '10mb',
+        },
     },
-  },
-}
+};
